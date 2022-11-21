4e694a0813f9fdf6a3f3b5c1afcd94054934577f
XuanKien-Nguyen <kienpc9x@gmail.com>
2022-11-21 15:52:20 +0700

    user.service

--- src/services/api.js
+++ src/services/api.js
@@ -4,20 +4,47 @@ import TokenService from "./token.service";
 const instance = axios.create({
     baseURL: "http://localhost:8080/api",
     headers: {
-        "Content-Type": "application/json",
-    },
+        "Content-Type": "application/json"
+    }
 });
 
 
-axios.interceptors.request.use(function (config) {
-    // Do something before request is sent
+axios.interceptors.request.use(function (config) { // Do something before request is sent
     const token = TokenService.getLocalAccessToken();
-    if (token) {
-      // config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
-      config.headers["x-access-token"] = token; // for Node.js Express back-end
+    if (token) { // config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
+        config.headers["x-access-token"] = token; // for Node.js Express back-end
     }
     return config;
-  }, function (error) {
-    // Do something with request error
+}, function (error) { // Do something with request error
     return Promise.reject(error);
-  });
+});
+
+axios.interceptors.response.use(
+    (res) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return res;
+}, 
+    async (err) => {
+    const originalConfig = err.config;
+
+    if (originalConfig.url !== '/auth/signin' && err.response) {
+        originalConfig._retry = true;
+
+        try {
+            const rs = await instance.post("/auth/refreshtoken", {
+                refreshToken: TokenService.getLocalRefreshToken(),
+            });
+
+            const {accessToken} = rs.data;
+            TokenService.updateLocalAccessToken(accessToken);
+
+            return instance(originalConfig);
+
+        } catch (_error){
+            return Promise.reject(_error);
+        }
+    }
+    
+    return Promise.reject(err);
+});
---
+++ src/services/auth.service.js
@@ -0,0 +1,22 @@
+import api from './api';
+import TokenService from "./token.service";
+
+class AuthService {
+    login() {
+
+    }
+
+    logout() {
+
+    }
+
+    register() {
+
+    }
+
+    getCurrentUser() {
+        
+    }
+}
+
+export default new AuthService;
